8ed4e0cb60aacd6e73bb36b9a53d8ff5756fc683
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-04-27 18:07:16 -0300

    feat: place control

---
+++ apps/front-end/functions/place.js
@@ -0,0 +1,40 @@
+let place = {
+    name: "main",
+    scene: "door",
+    objecs: [
+
+    ],
+}
+
+function setPlace(name){
+    place.name = name;
+}
+
+function getPlace(){
+    return place.name;
+}
+
+function setScene(scene){
+    place.scene = scene;
+}
+
+function getScene(){
+    return place.scene;
+}
+
+function setObjects(objects){
+    place.objecs = objects;
+}
+
+function getObjects(){
+    return place.objecs;
+}
+
+module.exports = {
+    setPlace,
+    getPlace,
+    setScene,
+    getScene,
+    setObjects,
+    getObjects,
+}
