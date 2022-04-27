2d41a1fdc77b390cea5a56c12354245075475af2
PedroLuisBrilhadori <74775672+PedroLuisBrilhadori@users.noreply.github.com>
2022-04-27 08:08:43 -0300

    feat: express sever (#4)
    * refactor: change name for images
    
    * refactor: rename images
    
    * feat: list objects and places
    
    * feat: get scenes arrays
    
    * feat: search file path

---
+++ apps/back-end/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+const list = require('./files');
+
+app.use('/assets',express.static(`${__dirname}/assets`));
+
+app.get('/:place/:scene?/objects', (req, res) => {
+    if(!req.params.place) {
+        res.send("Parametros não preenchidos corretamentes");
+        return;
+    }
+
+    (list.objects(req.params.place, req.params.scene).then( (a) => {
+        res.json({ 
+            objects: a
+        });
+    }));
+});
+
+app.get('/places', (req, res) => {
+    (list.places().then( (a) => {
+        res.json({ 
+            places: a
+        });
+    }));
+});
+
+app.get('/:place/scenes', (req, res) => {
+    if(!req.params.place) {
+        res.send("Parametros não preenchidos corretamentes");
+        return;
+    }
+
+    (list.scenes(req.params.place).then( (a) => {
+        res.json({ 
+            scenes: a
+        });
+    }));
+});
+
+app.get('/:file?/path', (req, res) => {
+    (list.path(req.params.file).then( (a) => {
+        res.json({ 
+            files: a
+        });
+    }));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
