4d0db2c3073574522c78b936d62ce4e0da0a6a5f
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-04-27 18:06:51 -0300

    refactor: async method places

--- apps/front-end/functions/http.js
+++ apps/front-end/functions/http.js
@@ -1,15 +1,34 @@
 const url = 'https://my-dream-house-fatec.herokuapp.com';
+const headers = {
+    'Content-Type': 'application/json',
+}
 
 
-function places() {
-    return fetch(`${url}/places`, {
-        headers: {
-            'Content-Type': 'application/json',
-        }
+async function places() {
+    return await fetch(`${url}/places`, {
+        headers: headers
     })
+    .then(response => response.json())
+    .then(data => {
+        return data.places;
+    })
+    .catch(error => console.error(error));
+}
+
+async function search(param) {
+    return await fetch(`${url}/${param}/path`, {
+        headers: headers,
+    })
+    .then(response => response.json())
+    .then(data => {
+        return data.files;
+    })
+    .catch(error => console.error(error));
 }
 
 
 module.exports = {
     places,
+    search,
+    url
 }
