9ec354652ad2a182cb79a06a8db1fa3ae065a3e2
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-04-27 15:54:23 -0300

    fix: webpack and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,5 @@ module.exports = {
   content: ["./apps/front-end/**/*.{html,js}"],
   theme: {
     extend: {},
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, '/apps/front-end'),
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-    ],
-  },
+  }
 }
