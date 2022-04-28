b45142e979297f1bc70a108fab239ac1ca036936
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-04-28 09:31:38 -0300

    feat: get all scenes and paths

--- apps/front-end/functions/menu.js
+++ apps/front-end/functions/menu.js
@@ -1,10 +1,56 @@
 const get = require('./http');
+const place = require('./place');
+const grid = document.getElementById('cardGrid')
+
+const menuContext = {
+    scenes: [],
+    page: 0,
+    pageSize: 8,
+}
+
+async function getScenes(){ 
+    return await get.scenes().then(data => {
+        return data
+    });
+}
+
+async function constructPlace(){
+    return await getScenes().then( data => {
+        data.forEach(scene => {
+            const current = {
+                name: getSceneName(scene),
+                path: scene,
+            }
+
+            menuContext.scenes.push(current);
+        });
+    })
+}
 
 function showPlaces(){
-   get.places().then(places => console.log(places));
+    constructPlace().then(a => {
+        let scene = document.createElement('div');
+    })
 }
 
 
 module.exports = { 
     showPlaces
+}
+
+
+function getSceneName(obj){    
+    let object = String(obj);
+
+    if(!object.includes('.jpg')){
+        return obj;
+    }
+
+    let name = '';
+    
+    for(let i = object.indexOf('.jpg') -1; object[i] !== '/'; i--) {
+        name += object[i];   
+    }
+
+    return name.split('').reverse().join('');
 }
