ff01150b0c079ceea61336bb8b7bcc29c6c9770f
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-04-28 10:10:13 -0300

    feat: menu of scenes

--- apps/front-end/functions/menu.js
+++ apps/front-end/functions/menu.js
@@ -1,35 +1,104 @@
 const get = require('./http');
 const place = require('./place');
 const grid = document.getElementById('cardGrid')
+const main = document.getElementById('main')
+
+document.getElementById('arrowNext').addEventListener('click', () => {
+    menuContext.page = 2;
+    document.getElementById('0').classList = "";
+    document.getElementById('1').classList = "selected";
+    while(grid.childNodes.length){
+        grid.childNodes.forEach(child => {
+            grid.removeChild(child);
+        })
+    }
+
+    
+    showPlaces();
+});
+
+document.getElementById('arrowBack').addEventListener('click', () => {
+    menuContext.page = 0;
+    document.getElementById('0').classList = "selected";
+    document.getElementById('1').classList = "";
+    
+    while(grid.childNodes.length){
+        grid.childNodes.forEach(child => {
+            grid.removeChild(child);
+        })
+    }
+
+    showPlaces();
+})
 
 const menuContext = {
     scenes: [],
+    scenes2: [],
     page: 0,
-    pageSize: 8,
 }
 
 async function getScenes(){ 
     return await get.scenes().then(data => {
+        menuContext.scenes = [];
+        menuContext.scenes2 = [];
         return data
     });
 }
 
 async function constructPlace(){
     return await getScenes().then( data => {
+        let i = 1;
         data.forEach(scene => {
             const current = {
                 name: getSceneName(scene),
                 path: scene,
             }
 
-            menuContext.scenes.push(current);
+            if(i <= 8)
+                menuContext.scenes.push(current);
+            else 
+                menuContext.scenes2.push(current);
+            i++;
         });
     })
 }
 
 function showPlaces(){
     constructPlace().then(a => {
-        let scene = document.createElement('div');
+
+        if(menuContext.page === 0){
+            menuContext.scenes.forEach(s => {
+                let scene = document.createElement('div');
+            
+                let name = document.createElement('p');
+                name.innerText = `${s.name}`;
+    
+                scene.appendChild(name);
+    
+                scene.id = `$${s.name}`;
+                scene.classList = 'cardPlace';
+                scene.style.backgroundImage = `url(${get.url}/${s.path})`;
+                scene.style.backgroundSize = 'cover';
+    
+                grid.appendChild(scene);
+            })        
+        } else {
+            menuContext.scenes2.forEach(s => {
+                let scene = document.createElement('div');
+            
+                let name = document.createElement('p');
+                name.innerText = `${s.name}`;
+    
+                scene.appendChild(name);
+    
+                scene.id = `$${s.name}`;
+                scene.classList = 'cardPlace';
+                scene.style.backgroundImage = `url(${get.url}/${s.path})`;
+                scene.style.backgroundSize = 'cover';
+    
+                grid.appendChild(scene);
+            })  
+        }
     })
 }
 
