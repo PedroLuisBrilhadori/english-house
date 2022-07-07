8a4d5c4c572a7354d0dc208fc8839ce2a6df86b6
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-07-07 18:54:46 -0300

    fix: api url

--- src/functions/http.js
+++ src/functions/http.js
@@ -1,70 +1,69 @@
-// const url = 'https://my-dream-house-fatec.herokuapp.com';
-const url = 'http://localhost:3000';
+const url = "https://my-dream-house-fatec.herokuapp.com";
 
-const place = require('./place');
+const place = require("./place");
 
 const headers = {
-    'Content-Type': 'application/json',
-}
+    "Content-Type": "application/json",
+};
 
 async function places() {
     return await fetch(`${url}/places`, {
-        headers: headers
+        headers: headers,
     })
-    .then(response => response.json())
-    .then(data => {
-        return data.places;
-    })
-    .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+            return data.places;
+        })
+        .catch((error) => console.error(error));
 }
 
 async function search(param) {
     return await fetch(`${url}/${param}/path`, {
         headers: headers,
     })
-    .then(response => response.json())
-    .then(data => {
-        return data.files;
-    })
-    .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+            return data.files;
+        })
+        .catch((error) => console.error(error));
 }
 
-async function objects(place, scene = ''){
-    const objUrl = scene !== '' ? `${place}/${scene}/objects` : `${place}/objects`;
+async function objects(place, scene = "") {
+    const objUrl =
+        scene !== "" ? `${place}/${scene}/objects` : `${place}/objects`;
     return await fetch(`${url}/${objUrl}`, {
-        headers: headers
+        headers: headers,
     })
-    .then(response => response.json())
-    .then(data => {
-        return data.objects;
-    })
-    .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+            return data.objects;
+        })
+        .catch((error) => console.error(error));
 }
 
 async function scenes() {
     const sceneUrl = `scenes`;
 
     return await fetch(`${url}/${sceneUrl}`, {
-        headers: headers
+        headers: headers,
     })
-    .then(response => response.json())
-    .then(data => {
-        return data.scenes;
-    })
-    .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+            return data.scenes;
+        })
+        .catch((error) => console.error(error));
 }
 
 async function texts(scene) {
     const sceneUrl = `./assets/jsons/${scene}.json`;
 
     return await fetch(`${sceneUrl}`)
-    .then(response => response.json())
-    .then(data => {
-        return data;
-    })
-    .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => console.error(error));
 }
-
 
 module.exports = {
     url,
@@ -73,4 +72,4 @@ module.exports = {
     objects,
     scenes,
     texts,
-}
+};
